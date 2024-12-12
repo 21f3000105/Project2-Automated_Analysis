8c1cea9b0dfae59a4c75e1dffe7669c78e7e6645
21f3000105 <21f3000105@ds.study.iitm.ac.in>
2024-12-12 21:07:44 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,6 +1,6 @@
 # /// script
 # requires-python = ">=3.11"
-# dependencies = ["numpy", "pandas", "scikit-learn", "chardet", "requests", "seaborn", "matplotlib", "python-dotenv"]
+# dependencies = ["numpy", "pandas", "scikit-learn", "chardet", "requests", "seaborn", "matplotlib", "python-dotenv","httpx"]
 # ///
 
 # Import necessary libraries after ensuring they're installed
